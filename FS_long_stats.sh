c37fb2c5a4ab047f6d4fff516b44e438746838cd
j-russell-34 <jason.russell34@gmail.com>
2024-11-13 16:39:06 -0800

    updated file paths, extract subcortical volumes form longitudinal fs

--- FS_long_stats.sh
+++ FS_long_stats.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 #setup FS
+export FREESURFER_HOME=/Applications/freesurfer/7.4.1
+export out_dir=/Users/jasonrussell/Documents/OUTPUTS/fslong_A001
+export in_dir=/Users/jasonrussell/Documents/INPUTS/fslong_A001/resources
+export SUBJECTS_DIR=$in_dir/SUBJECTS
 source $FREESURFER_HOME/SetUpFreeSurfer.sh
+cd /Users/jasonrussell/Documents/INPUTS/fslong_A001
 
 #generate subcortical stats table
-asegstats2table --qdec-long long.qdec.table.dat --stats aseg.stats --tablefile aseg.table.txt
+asegstats2table --qdec-long $out_dir/long.qdec.table_test.dat --stats aseg.stats --tablefile $out_dir/aseg.table.txt
+
+
