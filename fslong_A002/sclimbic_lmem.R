4e0d170be4cbcc8527b688739082a65486367fd4
j-russell-34 <jason.russell34@gmail.com>
2024-11-25 15:37:51 -0800

    R script to perform lmem on averaged sclimbic data and plot age vs BFV/TIV

---
+++ fslong_A002/sclimbic_lmem.R
@@ -0,0 +1,44 @@
+library(tidyverse)
+library(dplyr)
+library(purrr)
+library(glue)
+
+in_dir <- "/Users/jasonrussell/Documents/INPUTS/fslong_A002"
+out_dir <- "/Users/jasonrussell/Documents/OUTPUTS/fslong_A002"
+
+#import qdec and sclimbic data
+qdec <- read.table(glue("{out_dir}/long.qdec.table_test.dat"), header = TRUE)
+sclimbic <- read.csv(glue("{in_dir}/sclimbic_long_dummy.csv"))
+
+#set up linear mixed effects models
+library(lme4)
+library(lmerTest)
+library(sjPlot)
+
+#combine dataframes based on subject_id/event code
+data <- merge(qdec, sclimbic, by.x="fsid", by.y="id")
+data$basal_forebrain <- 
+  (data$left_basal_forebrain + data$right_basal_forebrain)/2
+
+
+model <- lmer(
+  basal_forebrain ~ years * age + eTIV + (1 | fsid.base),
+  data = data
+)
+
+summary(model)
+
+#plot graph x-axis age, y-axis basal_forebrain volume
+# Plot basal forebrain volume over time by age
+
+#data$predicted <- predict(model)
+
+ggplot(data, aes(x = age, y = basal_forebrain/eTIV, color = as.factor(fsid.base))) +
+  geom_point() +
+  geom_line(aes(group = fsid.base)) +
+  #geom_line(aes(y = predicted)) +
+  labs(title = "Effect of Age on Basal Forebrain Volume Change", 
+       x = "Age", 
+       y = "Basal Forebrain Volume/TIV") +
+  theme_minimal() +
+  theme(legend.position = "none")
